b3dab3ea311df183f5eac58c2b406050c21ff51d
NourBesbes <nourbesbes95@gmail.com>
2017-03-23 18:18:53 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {FacebookService, FacebookInitParams} from 'ng2-facebook-sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app works!';
+
+constructor(private fb: FacebookService) {
+	let fbParams: FacebookInitParams = {
+                                   appId: '251566128622296',
+                                   xfbml: true,
+                                   version: 'v2.8'
+                                   };
+    this.fb.init(fbParams);
+    
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {FacebookService, FacebookInitParams} from 'ng2-facebook-sdk';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [FacebookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
